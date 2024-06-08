d8a483507029c44e56e4b54663d02a876fd32fcc
Edward Olsen <eddiecode100@gmail.com>
2024-06-08 02:52:10 -0700

    prototyping member class functions. 3 am. time for bed.

--- include/item_class.h
+++ include/item_class.h
@@ -5,13 +5,37 @@
 #include <list>
 #include <random>
 
-class todo_item {
-  private:
-    int id_;
-    std::string desc_;
+class Item {
+    private:
+        int id_;
+        std::string description_;
+        bool completed_;
+    
+    public:
+        Item() : id_(0), description_(""), completed_(false) {}
+       
+        ~Item: default;
+           
+        // main functions
+
+        bool create(std::string&);
+
+        int generateID();
+
+        void clear() const;
 
 
+        // getters
 
+        void getID() const;
 
+        void getDesc() const;
 
+        void getStatus() const;
+
+        // setters
+
+        std::string setDesc(std::string&);
+        
+        bool setStatus(const bool&);
 };
